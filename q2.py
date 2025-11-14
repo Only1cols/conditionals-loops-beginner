aa5e4a25ac2163252c85a163d019487e413d17c4
only1cols <emmanuelebuka780@gmail.com>
2025-11-14 01:31:05 +0100

    updated guessing number algorithm

--- q2.py
+++ q2.py
@@ -1,10 +1,10 @@
 # guess game using while loop
 secret_number = 10
-guess = int(input("guess a number between 1 and 10: "))
-while guess != secret_number:
-    print("incorrect, try again")
-    guess = int(input("guess a number between 1 and 10: "))
 
+while True:
+    guess = int(input("guess a number between 1 and 10: "))
     if guess == secret_number:
         print("correct!!!")
         break
+    else:
+        print("Incorrect")
